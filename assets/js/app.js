9e74f26dea78d8c8007934c7ec6351b3b4bb7dbb
orMan <orman_macbook@orMans-MacBook-Air.local>
2023-02-14 23:54:01 +0330

    app.js Cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,156 +1,179 @@
 "use strict";
 /* l00p Studio - l00p.dev */
 
-document.querySelectorAll('.cm_input__floating').forEach( (el) => {
-    let input = el.querySelector('input, textarea')
-    checkInputState(input)
-
-    function checkInputState(e) {
-        if(input.value.length === 0) {
-            e.classList.remove('valuable')
-        }
-        else {
-            e.classList.add('valuable')
-        }
-    }
-
-    input.addEventListener('change', (event)=> {
-        checkInputState(event.target)
-    })
-
-    input.addEventListener('input', (event)=> {
-        checkInputState(event.target)
-    })
-})
-
-document.querySelectorAll('.cm_input__password').forEach((el) => {
-    let input = el.querySelector('input')
-    let button = el.querySelector('button')
-
-    button.parentComponent = el
-    button.inputComponent = input
-
-    button.addEventListener('click', (event) => {
-        let parent = event.currentTarget.parentComponent
-        let input = event.currentTarget.inputComponent
-
-        switch (input.type) {
-            case 'text': {
-                input.type = "password"
-                parent.classList.remove('show-password')
-            } break;
-
-            case 'password' : {
-                input.type = "text"
-                parent.classList.add('show-password')
-            } break;
-        }
-    })
-})
-
-document.querySelectorAll('video').forEach(video => {
-    new Plyr(video, {
-        settings: [],
-        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'pip', 'fullscreen'],
-        //ratio: '1:1'
-    });
-})
-
-document.querySelectorAll('audio').forEach(audio => {
-    new Plyr(audio, {
-        settings: [],
-        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
-    });
-})
-
-document.querySelectorAll('.cm_tab').forEach(tab => {
-    tab.querySelectorAll(' .cm_tab__nav .cm_tab__nav__item').forEach((navItem, index) => {
-
-        navItem.addEventListener('click', event=> {
-            tab.querySelectorAll('.cm_tab__content__item, .cm_tab__nav__item').forEach(item => {
-                item.classList.remove('active')
-            })
-
-            tab.querySelector('.cm_tab__content__item:nth-child(' + (index + 1) + ')').classList.add('active')
-            event.target.classList.add('active')
-        })
-    })
-})
-
-document.querySelectorAll('.cm_faq__title').forEach(faqTitle => {
-    faqTitle.addEventListener('click', event => {
-        let faq = event.target.parentNode
-        let faqContent = faq.querySelector('.cm_faq__content')
-
-        if (!faq.classList.contains('open')) {
-            expandSection(faqContent)
-            faq.classList.add('open')
-
-            if(faq.classList.contains('king')) {
-                document.querySelectorAll('.cm_faq').forEach(item => {
-                    const itemGroup = item.dataset.group
-                    const currentFaqGroup = faq.dataset.group
-
-                    if(item !== faq && item.classList.contains('open') && itemGroup === currentFaqGroup) {
-                        collapseSection(item.querySelector('.cm_faq__content'))
-                        item.classList.remove('open')
-                    }
-                    else {
-                        console.log('this is not true')
-                    }
-                })
+// Component Functinos
+function componentFloatingInputInit() {
+    document.querySelectorAll('.cm_input__floating').forEach( (el) => {
+        let input = el.querySelector('input, textarea')
+        checkInputState(input)
+    
+        function checkInputState(e) {
+            if(input.value.length === 0) {
+                e.classList.remove('valuable')
+            }
+            else {
+                e.classList.add('valuable')
             }
         }
-        else {
-            collapseSection(faqContent)
-            faq.classList.remove('open')
-        }
-    })
-})
-
-document.querySelectorAll('.cm_select').forEach(select => {
-    const input = select.querySelector('input')
-
-    const selectInput = select.querySelector('.cm_select__input')
-    const selectDropdown = select.querySelector('.cm_select__dropdown')
-    const selectDropdownItems = select.querySelectorAll('.cm_select__dropdown__item')
-    selectInput.dropdown = selectDropdown
-
-    selectInput.addEventListener('click', event => {
-        if(event.currentTarget.dropdown.style.display === 'block') {
-            event.currentTarget.dropdown.style.display = 'none'
-            event.target.blur()
-        }
-        else {
-            event.currentTarget.dropdown.style.display = 'block'
-        }
-    })
-
-    selectInput.addEventListener('blur', event => {
-        const dropdown = event.currentTarget.dropdown
-
-        setTimeout(() => {
-            dropdown.style.display = 'none'
-        }, 100)
-    })
-
-    selectDropdownItems.forEach(dropDownItem => {
-        console.log(dropDownItem);
-        dropDownItem.data_input = input
-        dropDownItem.data_text = selectInput.querySelector('.cm_select__text')
-
-        dropDownItem.addEventListener('click', event => {
-            const value = event.target.dataset.value
-            event.currentTarget.data_input.value = value
-            event.currentTarget.data_text.innerHTML = event.target.innerHTML
+    
+        input.addEventListener('change', (event)=> {
+            checkInputState(event.target)
+        })
+    
+        input.addEventListener('input', (event)=> {
+            checkInputState(event.target)
         })
     })
-})
+}
+
+function componentPasswordInputInit() {
+    document.querySelectorAll('.cm_input__password').forEach((el) => {
+        let input = el.querySelector('input')
+        let button = el.querySelector('button')
+    
+        button.parentComponent = el
+        button.inputComponent = input
+    
+        button.addEventListener('click', (event) => {
+            let parent = event.currentTarget.parentComponent
+            let input = event.currentTarget.inputComponent
+    
+            switch (input.type) {
+                case 'text': {
+                    input.type = "password"
+                    parent.classList.remove('show-password')
+                } break;
+    
+                case 'password' : {
+                    input.type = "text"
+                    parent.classList.add('show-password')
+                } break;
+            }
+        })
+    })
+}
+
+function componentTabInit() {
+    document.querySelectorAll('.cm_tab').forEach(tab => {
+        tab.querySelectorAll(' .cm_tab__nav .cm_tab__nav__item').forEach((navItem, index) => {
+    
+            navItem.addEventListener('click', event=> {
+                tab.querySelectorAll('.cm_tab__content__item, .cm_tab__nav__item').forEach(item => {
+                    item.classList.remove('active')
+                })
+    
+                tab.querySelector('.cm_tab__content__item:nth-child(' + (index + 1) + ')').classList.add('active')
+                event.target.classList.add('active')
+            })
+        })
+    })
+}
+
+function componentFAQInit() {
+    document.querySelectorAll('.cm_faq__title').forEach(faqTitle => {
+        faqTitle.addEventListener('click', event => {
+            let faq = event.target.parentNode
+            let faqContent = faq.querySelector('.cm_faq__content')
+    
+            if (!faq.classList.contains('open')) {
+                expandSection(faqContent)
+                faq.classList.add('open')
+    
+                if(faq.classList.contains('king')) {
+                    document.querySelectorAll('.cm_faq').forEach(item => {
+                        const itemGroup = item.dataset.group
+                        const currentFaqGroup = faq.dataset.group
+    
+                        if(item !== faq && item.classList.contains('open') && itemGroup === currentFaqGroup) {
+                            collapseSection(item.querySelector('.cm_faq__content'))
+                            item.classList.remove('open')
+                        }
+                        else {
+                            console.log('this is not true')
+                        }
+                    })
+                }
+            }
+            else {
+                collapseSection(faqContent)
+                faq.classList.remove('open')
+            }
+        })
+    })
+}
+
+function componentSelectInit() {
+    document.querySelectorAll('.cm_select').forEach(select => {
+        const input = select.querySelector('input')
+    
+        const selectInput = select.querySelector('.cm_select__input')
+        const selectDropdown = select.querySelector('.cm_select__dropdown')
+        const selectDropdownItems = select.querySelectorAll('.cm_select__dropdown__item')
+        selectInput.dropdown = selectDropdown
+    
+        selectInput.addEventListener('click', event => {
+            if(event.currentTarget.dropdown.style.display === 'block') {
+                event.currentTarget.dropdown.style.display = 'none'
+                event.target.blur()
+            }
+            else {
+                event.currentTarget.dropdown.style.display = 'block'
+            }
+        })
+    
+        selectInput.addEventListener('blur', event => {
+            const dropdown = event.currentTarget.dropdown
+    
+            setTimeout(() => {
+                dropdown.style.display = 'none'
+            }, 100)
+        })
+    
+        selectDropdownItems.forEach(dropDownItem => {
+            console.log(dropDownItem);
+            dropDownItem.data_input = input
+            dropDownItem.data_text = selectInput.querySelector('.cm_select__text')
+    
+            dropDownItem.addEventListener('click', event => {
+                const value = event.target.dataset.value
+                event.currentTarget.data_input.value = value
+                event.currentTarget.data_text.innerHTML = event.target.innerHTML
+            })
+        })
+    })
+}
+
+function componentAudioInit() {
+    document.querySelectorAll('audio').forEach(audio => {
+        new Plyr(audio, {
+            settings: [],
+            controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
+        });
+    })
+    
+}
+
+function componentVideoInit() {
+    document.querySelectorAll('video').forEach(video => {
+        new Plyr(video, {
+            settings: [],
+            controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'pip', 'fullscreen'],
+            //ratio: '1:1'
+        });
+    })
+}
 
 // Initis
 AOS.init();
-
 autosize(document.querySelectorAll('textarea'))
+componentFloatingInputInit()
+componentPasswordInputInit()
+componentTabInit()
+componentFAQInit()
+componentSelectInit()
+componentAudioInit()
+componentVideoInit()
+
 
 iziToast.settings({
     icon: '',
@@ -222,7 +245,7 @@ function socialNetwordShare(to, url = window.location.href, text = '') {
     }
 }
 
-// Examples
+// Temporary
 const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
